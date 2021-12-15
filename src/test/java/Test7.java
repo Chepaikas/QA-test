c497edaca265273307e1c98720fe8ef3998e299d
Chepaikas <chepaikas@gmail.com>
2021-12-15 19:27:44 +0600

    изменил условия цикла

--- src/test/java/Test7.java
+++ src/test/java/Test7.java
@@ -15,17 +15,21 @@ public class Test7 {
     public void start(){
         driver = new ChromeDriver();
     }
+
     @Test
     public void test7(){
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        for (int x = 0; x < driver.findElements(cssSelector("#box-apps-menu #app-")).size(); x++){
+
+        int xSize = driver.findElements(cssSelector("#box-apps-menu #app-")).size();
+        for (int x = 0; x < xSize; x++){
             driver.findElements(cssSelector("#box-apps-menu #app-")).get(x).click();
             driver.findElement(By.cssSelector("h1"));
             if(driver.findElements(By.cssSelector("#box-apps-menu #app- .docs")).size()!=0){
-                for(int y = 0; y < driver.findElements(By.cssSelector("#box-apps-menu #app- .docs li")).size(); y++){
+                int ySize = driver.findElements(By.cssSelector("#box-apps-menu #app- .docs li")).size();
+                for(int y = 0; y < ySize; y++){
                     driver.findElements(By.cssSelector("#box-apps-menu #app- .docs li")).get(y).click();
                     driver.findElement(By.cssSelector("h1"));
                 }
@@ -38,4 +42,4 @@ public class Test7 {
         driver = null;
     }
 }
-//
+
